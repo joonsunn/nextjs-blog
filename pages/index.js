c4ed7a15173adb388305f6ec1ea7d09cf25b304b
Foo Joon Sunn <joonsunn.foo@gmail.com>
2023-11-08 15:16:57 +0800

    Edit intro to test Vercel CI/CD.

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>[Your Self Introduction]</p>
+                <p>[Your Self Introduction To The World.]</p>
                 <p>
                     (This is a sample website - you'll be building a site like
                     this one on{" "}
